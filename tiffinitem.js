25e50d0b15818b89c37269cf9d8785a85ba6ff35
Shivam Kotalia <110277970+Shivamkotalia2002@users.noreply.github.com>
2023-06-14 13:04:50 +0530

    Inital commits

---
+++ tiffinitem.js
@@ -0,0 +1,141 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Single Meal',
+    category : 'Sharma Dhaba',
+    rating : 4.7,
+    price: 300,
+    img: 'images/sharma/s-1.jpeg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Monthly',
+    category : 'Sharma Dhaba',
+    rating : 4.5,
+    price: 5000,
+    img: 'images/sharma/s-2.jpeg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Quaterly',
+    category : 'Sharma Dhaba',
+    rating : 4.8,
+    price: 12000,
+    img: 'images/sharma/s-3.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Single  Meal',
+    category : 'Paresh Dhaba',
+    rating : 4.1,
+    price: 200,
+    img: 'images/paresh/p-1.jpeg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Monthly',
+    category : 'Paresh Dhaba',
+    rating : 4.7,
+    price: 4500,
+    img: 'images/paresh/p-2.jpeg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Quaterly',
+    category : 'Paresh Dhaba',
+    rating : 4.2,
+    price: 11000,
+    img: 'images/paresh/p-3.jpeg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Single Meal',
+    category :'Hungry Vibes',
+    rating : 4.1,
+    price: 275,
+    img: 'images/hungry/h-1.jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Monthly',
+    category : 'Hungry Vibes',
+    rating : 3.3,
+    price: 3000,
+    img: 'images/hungry/h-2.jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Quaterly',
+    category : 'Hungry Vibes',
+    rating : 3.5,
+    price: 8000,
+    img: 'images/hungry/h-3.jpeg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Single Meal',
+    category : 'Dabba Mania',
+    rating : 4.1,
+    price: 400,
+    img: 'images/dabba/d-1.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Monthly',
+    category : 'Dabba Mania',
+    rating : 4.3,
+    price: 6500,
+    img: 'images/dabba/d-2.jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Quaterly',
+    category : 'Dabba Mania',
+    rating : 4.7,
+    price: 17500,
+    img: 'images/dabba/d-3.jpeg',
+    quantity: 1
+
+},
+{
+    id: 13,
+    name: 'Single Meal',
+    category : 'Kotalia Services',
+    rating : 3.9,
+    price: 375,
+    img: 'images/kotalia/k-1.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Monthly',
+    category : 'Kotalia Services',
+    rating : 3.8,
+    price: 5500,
+    img: 'images/kotalia/k-2.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Premium',
+    category : 'Kotalia Services',
+    rating : 4.4,
+    price: 15000,
+    img: 'images/kotalia/k-3.jpeg',
+    quantity: 1
+
+},
+]
+
+export {foodItem};
